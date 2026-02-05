c9ff1b1011bcca240f854072434ea7542890e538
Umer-fareed21 <madfareed16@gmail.com>
2026-02-05 12:36:20 +0500

    bot commit 1 c8bb61900b69ae02

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ const crypto = require("crypto");
 const gitPath = "C:\\Program Files\\Git\\bin\\git.exe";
 
 // pehle chota number rakho test ke liye
-const totalCommits = 1000;
+const totalCommits = 500;
 
 // file jisme activity likhi jaegi
 const fileName = "activity.txt";
